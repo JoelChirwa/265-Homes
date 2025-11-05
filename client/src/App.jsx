49c96e8ed6f52058e584e5eceb2b1c86d469c246
joelchirwa <joelchirwa@my.uopeople.edu>
2025-11-05 22:12:41 +0200

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-amber-300 font-bold ">265 Homes</h1>
+      <p className="text-blue-400 font-stretch-ultra-expanded">Welcome to 265 Homes</p>
+    </>
+  )
+}
+
+export default App
