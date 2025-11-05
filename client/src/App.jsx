8075d5bc88f6c19d458dc5768b7b553928b8c77b
joelchirwa <joelchirwa@my.uopeople.edu>
2025-11-05 23:23:28 +0200

    create header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import About from './pages/About'
 import SignIn from './pages/SignIn'
 import Profile from './pages/Profile'
 import SignUp from './pages/SignUp'
+import Header from './components/Header'
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
 
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import { FaSearch } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div>
-      <h1 className="text-amber-300 font-bold ">265 Homes</h1>
-      <p className="text-blue-400 font-stretch-ultra-expanded">Your trusted real estate partner</p>
-    </div>
+    <header className='bg-slate-200 shadow-md'>
+      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        <Link to="/">
+          <h1 className='text-sm sm:text-3xl font-bold flex flex-wrap'>
+            <span className='text-amber-700'>265</span>
+            <span className='text-black-400'>Homes</span>
+          </h1>
+        </Link>
+
+        <form className='bg-slate-100 p-1 rounded-lg flex items-center'>
+          <input type="text" placeholder="Search..." className='bg-transparent focus:outline-none w-24 sm:w-64' />
+          <FaSearch className='text-slate-500' />
+        </form>
+
+        <ul className='flex space-x-4 font-bold'>
+          <Link to="/" className='hidden sm:inline text-slate-900 hover:underline'>Home</Link>
+          <Link to="/about" className='hidden sm:inline text-slate-900 hover:underline'>About</Link>
+          <Link to="/signin" className='text-slate-900 hover:underline'>Sign In</Link>
+        </ul>
+      </div>
+
+
+    </header>
+      
+   
   )
 }
 
