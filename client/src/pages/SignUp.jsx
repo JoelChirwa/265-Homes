31670019af600fbcaa03a3eceac1a611438a258b
joelchirwa <joelchirwa@my.uopeople.edu>
2025-11-07 13:01:19 +0200

    complete sign up UI

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
   return (
-    <div>
-      <h1 className="text-amber-300 font-bold ">Sign Up</h1>
-      <p className="text-blue-400 font-stretch-ultra-expanded">Create a new account</p>
+    <div className='p-3 max-w-lg mx-auto'>
+       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+       <form className='flex flex-col gap-4'>
+          <input type="text" placeholder="Username" className='border p-3 rounded-lg' id='username' />
+          <input type="email" placeholder="Email" className='border p-3 rounded-lg' id='email' />
+          <input type="password" placeholder="Password" className='border p-3 rounded-lg' id='password' />
+          <button type="submit" className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-80'>Sign Up</button>
+       </form>
+       <div className='flex mt-5 gap-2'>
+        <p>Already have an account?</p>
+        <Link to="/log-in">
+          <span className='text-blue-700 hover:underline'>Log In</span>
+        </Link>
+       </div>
     </div>
   )
 }
